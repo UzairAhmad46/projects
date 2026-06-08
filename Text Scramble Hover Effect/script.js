c5bbdba94474e81621cc2cf7399d465217b305bc
UzairAhmad46 <uzairahmad46155@gmail.com>
2026-06-08 15:59:36 +0530

    projects

---
+++ Text Scramble Hover Effect/script.js
@@ -0,0 +1,24 @@
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+document.querySelector('h1').onmouseover = (e) => {
+  let iteration = 0;
+
+  const interval = setInterval(() => {
+    e.target.innerText = e.target.innerText
+      .split('')
+      .map((letter, index) => {
+        if (index < iteration) {
+          return e.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join('');
+
+    if (iteration >= e.target.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+};
